27e728034b58090a00503b78a02625aa994bced3
uttenatta <uttenatta@gmail.com>
2021-12-27 16:39:11 +0900

    second commit

--- study1.py
+++ study1.py
@@ -2,4 +2,12 @@ name = input("이름이 무엇입니까? : ")
 
 for i in range(10):
     print(name, end = "-")
-    
+
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)  
