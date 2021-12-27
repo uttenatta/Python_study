6d1ec94638eab0f88bea021c60806551720db01e
uttenatta <uttenatta@gmail.com>
2021-12-27 16:32:44 +0900

    first commit

---
+++ study1.py
@@ -0,0 +1,5 @@
+name = input("이름이 무엇입니까? : ")
+
+for i in range(10):
+    print(name, end = "-")
+    
---
+++ study2.py
@@ -0,0 +1,9 @@
+name = input("이름이 무엇입니까? : ")
+
+for i in range(10):
+    print(name, end = "-")
+    
+print(name)
+print(name)
+print(name)
+print(name)
